2abd4df36153ddda2033a0fd1ad163e292cf3708
chanminkim-K <snoopy0813@naver.com>
2024-07-08 15:12:13 +0900

    [react] Create 기능 완성

--- react/project2/todo-list-app/components/ToDoEditor.jsx
+++ react/project2/todo-list-app/components/ToDoEditor.jsx
@@ -1,12 +1,40 @@
+import { useRef, useState } from "react";
 import "./ToDoEditor.css"
 
-const ToDoEditor = () => {
+const ToDoEditor = ({ onCreate }) => { // Props 객체 구조 분해 할당
+    const [content, setContent] = useState("");
+    const inputRef = useRef(); // 할 일 입력 폼을 제어할 inpuRef 객체 생성
+    const onChangeContent = (e) => {
+        setContent(e.target.value);
+    }
+
+    const onSubmit = () => {
+        if(!content){
+            inputRef.current.focus();
+            return;
+        }
+        onCreate(content);
+        setContent(""); // 새 아이템 추가 후, 입력 폼 초기화
+    }
+
+    /* Enter 키를 눌러 아이템 추가하기 */
+    const onKeyDown = (e) => {
+        if(e.keyCode === 13){
+            onSubmit();
+        }
+    }
+
     return (
         <div className="ToDoEditor">
             <h4>새로운 ToDo 작성하기 ✏️ </h4>
             <div className="editor_wrapper">
-                <input placeholder="새로운 Todo..." />
-                <button>추가</button>
+                <input
+                    ref={inputRef}
+                    value={content}
+                    onChange={onChangeContent} 
+                    onKeyDown={onKeyDown}
+                    placeholder="새로운 Todo..." />
+                <button onClick={onSubmit}>추가</button>
             </div>
         </div>
     )
--- react/project2/todo-list-app/src/App.jsx
+++ react/project2/todo-list-app/src/App.jsx
@@ -1,17 +1,80 @@
 import './App.css'
+import { useState, useRef } from "react";
 import Header from '../components/Header'
 import ToDoEditor from '../components/ToDoEditor'
 import ToDoList from '../components/ToDoList'
 
+/**
+ * 목 데이터 설정하기
+ *  - 목 데이터 : 모조품 데이터 = 테스트를 목적으로 사용하는 데이터
+ */
+const mockTodo = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "빨래 널기",
+    createdDate: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "노래 연습하기",
+    createdDate: new Date().getTime(),
+  },
+];
+
 function App() {
+  // 1. 할 일 아이템의 상태 관리
+  //    State 변수 todo의 기본값을 빈 배열로 초기화
+  // const [todo, setTodo] = useState([]); 
+
+  // 2. 목 데이터 설정
+  //    State 변수 todo의 기본값으로 목 데이터 전달
+  const [todo, setTodo] = useState(mockTodo); 
+
+  // 3. 아이템 추가 함수 만들기
+  //    <추가> 버튼 클릭시 App에 할 일 데이터 전달하고 추가 이벤트 발생했음을 알려야 함
+  const idRef = useRef(3);
+
+  const onCreate = (content) => {
+    const newItem =  {
+      id: idRef.current,
+      content,
+      isDone: false,
+      createdDate: new Date().getDate(),
+    };
+    setTodo([newItem, ...todo]);
+    idRef.current += 1;
+  }
+
+ 
 
   return (
     <div className='App'>
       <Header />
-      <ToDoEditor />
+      <ToDoEditor onCreate={onCreate}/>
       <ToDoList />
     </div>
   )
 }
 
+
+
+
 export default App
+/** 
+ * 컴포넌트 별 구현해야 할 기능
+ * 
+ *  - App : 할 일 데이터 관리하기
+ *  - Header : 오늘의 날짜 표시
+ *  - ToDoEditor : 새로운 할 일 아이템 생성 => Create
+ *  - ToDoList : 검색에 따라 필터링된 할 일 아이템 렌더링 => Read
+ *  - ToDoItem : 할 일 아이템의 수정 및 삭제 => Update, Delete
+ * 
+ */
